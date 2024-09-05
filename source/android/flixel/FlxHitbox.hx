df7ebad2202ca48c2d52d3eede127407601e8a6d
Gaby Wuz <155038635+LakerWuzzy@users.noreply.github.com>
2024-09-05 13:54:35 -0300

    Acho que troquei as cores

--- source/android/flixel/FlxHitbox.hx
+++ source/android/flixel/FlxHitbox.hx
@@ -40,10 +40,10 @@ class FlxHitbox extends FlxSpriteGroup
 		scrollFactor.set();
 
 		hitbox = new FlxSpriteGroup();
-		hitbox.add(add(buttonLeft = createHitbox(0, 0, 'left', 0xFFC473FF)));
-		hitbox.add(add(buttonDown = createHitbox(FlxG.width / 4, 0, 'down', 0xFF2FFFF4)));
+		hitbox.add(add(buttonLeft = createHitbox(0, 0, 'left', 0xFF)));
+		hitbox.add(add(buttonDown = createHitbox(FlxG.width / 4, 0, 'down', 0xFF7375FF)));
 		hitbox.add(add(buttonUp = createHitbox(FlxG.width / 2, 0, 'up', 0xFF2EFFAF)));
-		hitbox.add(add(buttonRight = createHitbox((FlxG.width / 2) + (FlxG.width / 4), 0, 'right', 0xFF7375FF)));
+		hitbox.add(add(buttonRight = createHitbox((FlxG.width / 2) + (FlxG.width / 4), 0, 'right', 0xFFC473FF)));
 		hitbox.scrollFactor.set();
 	}
 
